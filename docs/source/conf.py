be4e764080d6397fe7a4392a047ff5e2d5c5fa7a
sopanawit <41313215+sopanawit@users.noreply.github.com>
2021-10-26 11:13:01 +0700

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'iThesis'
+copyright = '2021, MHESI'
+author = 'Sopanawit Pi'
 
 release = '0.1'
 version = '0.1.0'
